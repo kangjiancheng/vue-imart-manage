03b31dd8230874602be81a1f7449f00b89befd99
康建城 <1063303665@qq.com>
2020-04-26 17:45:12 +0800

    dd

---
+++ src/components/Layout/MenuHeader/components/ImFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="page-footer">
+    <p>Copyright © 2020 Imart</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "im-footer"
+}
+</script>
+
+<style lang="scss" scoped>
+.page-footer {
+  text-align: center;
+  p {
+    margin: 0;
+    padding: 0;
+  }
+}
+</style>
